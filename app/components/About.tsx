d06e21cf291264a56ac011298525504029da82b5
Daniel Qian <danielqian@Daniels-MacBook-Pro-2.local>
2025-01-18 16:46:54 -0800

    Adding in picture and more css

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,14 +1,32 @@
 import styles from "../../styles/Bio.module.css";
 import Link from "./Link";
 import { LinkType } from "./types";
+import DanielQian from "../../public/danielqian.png";
+import Image from "@/node_modules/next/image";
 
 export default function About() {
-  return (
-    <div>
+  const header = (
+    <div className="flex flex-row pb-4 justify-center gap-8">
+      <Image
+        className="w-[30%] h-[30%] rounded-full justify-left"
+        src={DanielQian}
+        alt="Daniel"
+      />
+
+      <div className="items-center justify-right">
+        <h2>Hey, I&apos;m Daniel!</h2>
+        <p>A software engineer</p>
+        <p>Based in the Bay Area</p>
+      </div>
+    </div>
+  );
+
+  const description = (
+    <>
       <p>
-        Hey, I&apos;m Daniel! I&apos;m currently an engineer at{" "}
+        I&apos;m currently an engineer at{" "}
         <Link
-          href="https://about.google/"
+          href="https://about.google"
           cssID="google"
           linkType={LinkType.STRING}
         >
@@ -20,10 +38,33 @@ export default function About() {
             <span className={styles.l}>l</span>
             <span className={styles.e}>e</span>
           </span>{" "}
-          </Link>{" "}
+        </Link>{" "}
         working on Cloud SQL.
       </p>
-      <p>I graduated from UIUC, studying Mathematics and Computer Science with a Minor in Statistics.</p>
+      <p>
+        I graduated from{" "}
+        <Link
+          href="https://cs.illinois.edu"
+          cssID="uiuc-cs"
+          linkType={LinkType.STRING}
+        >
+          <span className={styles.uiuccs}>UIUC</span>
+        </Link>{" "}
+        studying Mathematics and Computer Science with a Minor in Statistics.
+      </p>
+    </>
+  );
+
+  return (
+    <div className="about-root">
+      {header}
+      <hr />
+      <div className="centered about-text-container">
+        {description}
+      </div>
+      {/* <div className="centered about-text-container">
+                {history}
+            </div> */}
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,12 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 items-center sm:items-start justify-center flex-grow p-8 sm:p-20">
-        <PageHome />
+      <main className="flex flex-col gap-8 items-center justify-center flex-grow p-8 sm:p-20">
         <About />
       </main>
-
+      <main className="flex flex-col gap-8 items-center justify-center flex-grow p-8 sm:p-20">
+        <PageHome />
+      </main>
       <footer className="bg-black text-white py-6 w-full">
         <Footer />
       </footer>
